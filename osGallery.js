8923f998e6d328737bc3fcfcfcaf3be7323ee004
Oğuzhan SARI <os@oguzhansari.com>
2015-12-18 12:37:11 +0200

    Dosyalar
    Dosya aktarımı

---
+++ osGallery.js
@@ -0,0 +1,253 @@
+﻿
+(function ($) {
+    $.osGallery = function (el, options) {
+        var gallery = $(el);
+        gallery.vars = $.extend({}, $.osGallery.defaults, options);
+        $.data(el, "osGallery", gallery);
+        osGmethods = {
+            initx: function () {
+                console.log("initx");
+                return $(this).each(function () {
+                    var active = 0;
+                    var id = codeGenerator(20);
+                    var t = gallery;
+                    var o = gallery.vars;
+                    t.addClass(id).attr("data-id", id);
+                    var length = $(o.imgElement + '[ data-view="true"]', t).length;
+                    $('body').append('<div class="osGallery ' + (length > 1 ? '' : 'oneImage') + '" id="' + id + '"><div class="navigation"><div class="navBox"><span class="title"></span><nav>' + (length > 1 ? '<a href="javascript:void(0)" data-arrows="left" class="osg osg-left"></a><a href="javascript:void(0)" data-arrows="right" class="osg osg-right"></a>' : '') + '<a href="javascript:void(0)" data-arrows="close" class="osg osg-cross"></a></nav></div></div><div class="galleryContent"><div class="galleryBox"></div></div>' + (length > 1 ? '<div class="gallerThumbnails"><nav><ul></ul></nav></div>' : '') + '</div>');
+                    var gt = $('#' + id);
+                    var gb = $('.galleryContent .galleryBox', gt);
+                    var includeDiv = $(o.includeDiv + '', t);
+                    //Image Generator +
+                    var i = 1;
+                    $(o.imgElement + '[ data-view="true"]' + ' img', t).each(function (index) {
+                        var iet = $(this);
+                        iet.parents(o.imgElement).attr('data-id', i)
+                        //Big Image
+                        var img = new Image();
+                        img.src = iet.attr("data-big-img");
+                        var figure = document.createElement('figure');
+                        figure.setAttribute('data-fid', i);
+                        figure.appendChild(img);
+                        figure.setAttribute('class', 'deactive');
+                        $('.galleryContent .galleryBox', gt).append(figure);
+                        //Thumbnail
+                        var img = new Image();
+                        img.src = iet.attr("src");
+                        img.setAttribute('class', "grayscale grayscale-fade");
+                        var li = document.createElement('li');
+                        li.setAttribute('data-tid', i);
+                        li.appendChild(img);
+                        $('.gallerThumbnails nav ul', gt).append(li);
+                        i = i + 1;
+                        $(this).css({ "display": "block" });
+                    });
+                    $(o.imgElement + '[ data-view="false"]', t).each(function (index) {
+                        $(this).css({ "display": "none" });
+                    });
+                    //Image Generator -
+
+                    //Click Functions +
+                    var c = $('.gallerThumbnails nav li', gt);
+                    c.click(function () {
+                        clicker($(this).attr('data-tid'), id);
+                    });
+                    var fc = $(o.imgElement + '[ data-view="true"]', t);
+                    if (o.galleryType == "include") {
+                        fc.click(function () {
+                            $(o.imgElement).removeClass("active");
+                            $(this).addClass("active");
+                            clickerInclude($(this), $(this).attr('data-id'), id);
+                        });
+                    } else {
+                        fc.click(function () {
+                            $(o.imgElement).removeClass("active");
+                            $(this).addClass("active");
+                            clicker($(this).attr('data-id'), id);
+                        });
+                    }
+					if (o.galleryType == "include") {
+						fc.eq(0).trigger('click');
+                    }
+                    function clickerInclude(t, a, id) {
+                        var i = t.attr("data-id");
+                        var t = $('img', t);
+                        var img = new Image();
+                        img.src = t.attr("data-big-img");
+                        img.setAttribute("data-tid", i)
+                        includeDiv.html(img);
+                        $('img', includeDiv).click(function () {
+                            clicker(i, id);
+                        });
+                    }
+
+                    $(gt).hover(function () {
+                        $("body").attr("onKeyDown", "$.doKey(event); return false;");
+                    }, function () {
+                        $("body").removeAttr("onKeyDown");
+                    });
+
+                    function clicker(a, id) {
+                        var openGallery = $('#' + id);
+                        if (!openGallery.hasClass('activeGallery')) {
+                            openGallery.fadeIn();
+                            openGallery.addClass('activeGallery');
+                        }
+                        $('.navigation span.title', openGallery).html($(o.imgElement + '[data-id="' + a + '"] img', t).attr('data-title'));
+                        setTimeout(function () {
+                            var thumb_t = $('.gallerThumbnails nav li[data-tid="' + a + '"]', openGallery);
+                            var thumb_all = $('.gallerThumbnails nav li[data-tid]', openGallery);
+                            var figure_all = $('[data-fid]', gb);
+                            var figure_t = $('[data-fid="' + a + '"]', gb);
+                            thumb_all.removeClass('tactive');
+                            $('img', thumb_all).addClass('grayscale grayscale-fade');
+                            thumb_t.addClass('tactive');
+                            $('img', thumb_t).removeClass('grayscale grayscale-fade');
+                            if (o.animateType == "animated") {
+                                var e = o.animatedEffect;
+                                $('[data-fid="' + active + '"] img', gb).removeClass().addClass(e[1] + ' animated');
+                                figure_t.removeClass('deactive').addClass('active');
+                                $('img', figure_t).removeClass().addClass(e[0] + ' animated');
+                            } else {
+                                $('img', figure_all).addClass('transition zoom');
+                                figure_all.addClass('deactive transition zoom').removeClass('active');
+                                figure_t.removeClass('deactive').addClass('active');
+                            }
+                            active = a;
+                        }, 0);
+                        if (o.galleryType == "include") {
+                            $(o.imgElement + '[data-id="' + a + '"] img', t).trigger('click');
+                        }
+                    }
+
+                    $('.navigation nav [data-arrows]', gt).click(function () {
+                        var type = $(this).attr('data-arrows');
+                        if (type == "close") {
+                            galleryclosed();
+                        } else if (type == "left") {
+                            var go = active == 1 ? length : parseInt(active) - 1;
+                            clicker(go, id);
+                        } else if (type == "right") {
+                            var go = active == length ? 1 : parseInt(active) + 1;
+                            clicker(go, id);
+                        }
+                    });
+                    //Click Functions -
+
+                    $('.galleryBox figure', gt).click(function (e) {
+                        var trgt = e.target;
+                        if (!$('img', gt).is(trgt)) {
+                            galleryclosed();
+                        }
+                    });
+
+                    function galleryclosed() {
+                        gt.removeClass('activeGallery');
+                        gt.fadeOut();
+                        setTimeout(function () {
+                            gt.removeClass('transition');
+                        }, 0);
+                        $('[data-fid]', gb).addClass('deactive').removeClass('active');
+                    }
+
+                    $.doKey = function (e) {
+                        if (e.keyCode == 37) {
+                            $('.navigation nav [data-arrows="left"]', gt).trigger("click");
+                        } else if (e.keyCode == 39) {
+                            $('.navigation nav [data-arrows="right"]', gt).trigger("click");
+                        }
+                        return false;
+                    }
+
+                });
+            }
+        }
+        gallery.destroy = function () {
+            var o = gallery.vars;
+            var t = gallery;
+            var id = t.attr("data-id");
+            var imgElement = $(o.imgElement, t);
+            t.removeAttr("data-id").removeClass(id);
+            $('#' + id).remove();
+            $("*", t).off("click");
+            imgElement.attr("data-view", "true");
+            imgElement.css("display", "block");
+            return true;
+        }
+        gallery.rebuild = function () {
+            gallery.destroy();
+            osGmethods.initx();
+        }
+        gallery.arrayrebuild = function (value) {
+            if (gallery.destroy()) {
+                var o = gallery.vars;
+                var t = gallery;
+                var imgElement = $(o.imgElement, t);
+                var arrayVal = parseInt(value);
+                imgElement.each(function (index) {
+                    var t = $(this);
+                    var split = $(this).attr('data-array-options').split(",");
+                    var splitLen = split.length;
+                    if ($(this).attr('data-array-options') == "" || $(this).attr('data-array-options') == undefined) {
+                        splitLen = 0;
+                    }
+                    var array = [];
+                    if (splitLen != 0) {
+                        for (var i = 0; i < splitLen; i++) {
+                            array.push(parseInt(split[i]));
+                        }
+                    }
+                    if (array.indexOf(arrayVal) > -1 || array.length == 0) {
+                        t.attr("data-view", "true");
+                    } else {
+                        t.attr("data-view", "false");
+                    }
+                });
+                console.log("arraybuild finish");
+                osGmethods.initx();
+                console.log("arraybuild finish 2");
+            }
+        }
+        gallery.initxreplay = function () {
+            osGmethods.initx();
+        }
+        osGmethods.initx();
+    }
+    $.osGallery.defaults = {
+        imgElement: 'figure',
+        animateSpeed: 1500,
+        animateType: 'animated', /* zoom, animated */
+        animatedEffect: ['zoomIn', 'zoomOut'],
+        galleryType: 'none', // include
+        includeDiv: '.viewer'
+    }
+    $.fn.osGallery = function (options, opt) {
+        if (options === undefined) { options = {}; } // object değer girilmemişse boş object tanımlıyoruz.
+        if (typeof options === "object") {
+            return this.each(function () {
+                options = $.extend({}, options, { BottomMenuID: codeGenerator(20) });
+                var $this = $(this);
+                if ($this.data('osGallery') === undefined) {
+                    new $.osGallery(this, options);
+                }
+            });
+        } else {
+            var $osGallery = $(this).data('osGallery');
+            if (typeof opt === "object") {
+                var arrayValue = opt.arrayValue;
+            }
+            switch (options) {
+                case "destroy": $osGallery.destroy(); break;
+                case "rebuild": $osGallery.rebuild(); break;
+                case "arrayrebuild": $osGallery.arrayrebuild(arrayValue); break;
+                default: $osGallery.destroy();
+            }
+        }
+    }
+})(jQuery);
+
+function codeGenerator(t) { void 0 == t && (t = 10); for (var o = "", n = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", s = 0; t > s; s++) o += n.charAt(Math.floor(Math.random() * n.length)); return o }
+
+$(function () {
+    $('#gallery').osGallery({ animateType: 'zoom' });
+});
---
+++ readme.md
@@ -0,0 +1,142 @@
+# osGallery
+
+jQuery ile gelişitirilmiş CSS3 ile güçlendirilmiş galeri uygulaması.
+
+### Özellikleri
+  - Açılır pencere galeri
+  - Direk olarak canlı izleme alanı ve açılır pencere galeri
+  - İlişkilendirmeli gösterim
+  - Destroy (sıfırlama)
+  - rebuild (yeniden oluşturma)
+  - arrayrebuild (iliklişi yeniden oluşturma)
+  - animate.css ile geçiş efektleri
+
+### Kullanımı
+Görüntülenmesini istediğin nesnenin ' data-view="true" ' tagına sahip olması gerekmektedir.
+
+#### 1) Include (Canlı izleme) özelliğinde galeri oluşturmak için
+```sh
+<div class="gallerystyle1" id="gallery1">
+    <div class="viewerBox">
+        <div class="viewer">
+            <img src="resim/buyuk_1.jpg" data-big-img="resim/buyuk_1.jpg" />
+        </div>
+    </div>
+    <ul class="thumbnails">
+        <li data-view="true"><figure><img src="resim/kucuk_4.jpg" data-big-img="resim/buyuk_4.jpg" /></figure></li>
+        <li data-view="true"><figure><img src="resim/kucuk_5.jpg" data-big-img="resim/buyuk_5.jpg" /></figure></li>
+        <li data-view="true"><figure><img src="resim/kucuk_8.jpg" data-big-img="resim/buyuk_8.jpg" /></figure></li>
+        <li data-view="true"><figure><img src="resim/kucuk_9.jpg" data-big-img="resim/buyuk_9.jpg" /></figure></li>
+    </ul>
+</div>
+<script>
+    $(document).ready(function () {
+        $('#gallery1').osGallery({ imgElement: 'li', galleryType: "include" });
+    });
+</script>
+<style>
+.gallerystyle1 { border: 2px solid #808080; width: 600px; margin: 0 auto; margin-top: 20px; }
+.gallerystyle1 * { list-style: none; }
+.gallerystyle1 img { max-width: 100%; }
+.gallerystyle1 .viewerBox { width: 100%; height: 380px; display: table; }
+.gallerystyle1 .viewerBox .viewer { cursor: pointer; display: table-cell; vertical-align: middle; }
+.gallerystyle1 .viewerBox .viewer img { display: block; }
+.gallerystyle1 .thumbnails { border-top: 1px solid #808080; }
+.gallerystyle1 .thumbnails:after { display: block; content: ""; clear: both; }
+.gallerystyle1 .thumbnails li { float: left; height: 80px; width: 20%; box-sizing: border-box; display: table; border-right: 1px solid #808080; border-bottom: 1px solid #808080; }
+.gallerystyle1 .thumbnails li figure { display: table-cell; vertical-align: middle; text-align: center; }
+.gallerystyle1 .thumbnails li figure img { max-height: 80%; max-width: 80%; margin: 0 auto; }
+.gallerystyle1 .thumbnails li:nth-of-type(5n+0) { border-right: 0; }
+</style>
+```
+
+#### 2) Açılır galeri özelliğinde oluşturmak için
+```sh
+<section class="gallerystyle2" id="gallery2">
+    <div class="item">
+        <figure data-view="true">
+            <span>
+                <img data-title="Resim Açıklama 1" src="resim/buyuk_1.jpg" data-big-img="resim/buyuk_1.jpg" class="grayscale grayscale-fade" />
+            </span>
+        </figure>
+        <figure data-view="true">
+            <span>
+                <img data-title="Resim Açıklama 1" src="resim/buyuk_2.jpg" data-big-img="resim/buyuk_2.jpg" class="grayscale grayscale-fade" />
+            </span>
+        </figure>
+        <figure data-view="true">
+            <span>
+                <img data-title="Resim Açıklama 1" src="resim/buyuk_3.jpg" data-big-img="resim/buyuk_3.jpg" class="grayscale grayscale-fade" />
+            </span>
+        </figure>
+        <figure data-view="true">
+            <span>
+                <img data-title="Resim Açıklama 1" src="resim/buyuk_6.jpg" data-big-img="resim/buyuk_6.jpg" class="grayscale grayscale-fade" />
+            </span>
+        </figure>
+        <figure data-view="true">
+            <span>
+                <img data-title="Resim Açıklama 1" src="resim/buyuk_7.jpg" data-big-img="resim/buyuk_7.jpg" class="grayscale grayscale-fade" />
+            </span>
+        </figure>
+	</div>
+</section>
+<script>
+    $(document).ready(function () {
+        $('#gallery2').osGallery();
+    });
+</script>
+<style>
+.gallerystyle2 { display: table; margin: 0 auto; max-width: 800px; padding-top: 20px; }
+.gallerystyle2 h3 { display: inline-block; font-size: 22px; color: #222222; text-shadow: 1px 1px #fff; padding: 10px; padding-left: 0; padding-bottom: 20px; position: relative; }
+.gallerystyle2 h3:after { content: ""; position: absolute; width: 40%; bottom: 14px; left: 0; background-color: #222222; height: 2px; }
+.gallerystyle2:after { content: ""; display: block; clear: both; }
+.gallerystyle2 div.item { display: table; border: 1px solid #808080; }
+.gallerystyle2 div.item figure { float: left; height: 80px; width: 20%; box-sizing: border-box; display: table; border-right: 1px solid #808080; border-bottom: 1px solid #808080; }
+.gallerystyle2 div.item figure span { display: table-cell; vertical-align: middle; text-align: center; }
+.gallerystyle2 div.item figure span img { max-height: 79px; max-width: 100%; margin: 0 auto; }
+.gallerystyle2 div.item figure:nth-of-type(5n+0) { border-right: 0; }
+</style>
+```
+
+#### 3) İlişkilendirmeli galeri
+İlişkilendirmeli galeri bir amaca yönelik oluşturuldu.
+Şu şekilde;
+    
+    Bir e-ticaret siteniz,
+    Bir ürün eklediniz ve ürününüze 10 tane resim girdiniz.
+    Ve ürünüze ait varyantlar mevcut.
+    Varyant girip eklediğiniz resimlerden varyanta ait olanları seçiyorsunuz.
+    Kullanıcı tarafında varyant seçimi yapan bir kullanıcıya galeride sadece o varyanta özel resimler gösteriliyor.
+    (Hiç bir varyanta eklenmeyecen resimlerde ortak resim olarak kabul edilir ve gösterilir.)
+    
+### Versiyon
+2.0
+
+### Teknik
+
+jQuery 2.1.4 ( http://www.jquery.com )
+Animate.css ( https://daneden.github.io/animate.css )
+Gray 1.4.5 ( https://github.com/karlhorky/gray )
+
+### Kurulum
+
+Aşağıdaki kodları <head> tagı içerisine yerleştirin.
+
+```sh
+<link href="animate.css" rel="stylesheet" />
+<link href="gray.min.css" rel="stylesheet" />
+<link href="osGallery.css" rel="stylesheet" />
+<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="osGallery.js"></script>
+```
+
+Yukarıdaki örnek kodları sitenize entegre edin ve kullanmaya başlayın.
+
+> Teşekkürler. :)
+
+> Oğuzhan SARI
+
+> os@oguzhansari.com
+
+> www.oguzhansari.com
